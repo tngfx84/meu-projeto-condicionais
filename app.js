89f1210f165bdaaf98e6bd6214316d33ca8985ef
Thaise França <150853168+tngfx84@users.noreply.github.com>
2024-11-29 11:33:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+diaDaSemana = prompt('Qual é o dia da semana?');
+if (diaDaSemana == 'Sábado') {
+    alert('Bom fim de semana!');
+} else if (diaDaSemana == 'Domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
+
+pontuacao = prompt('Diga sua pontuação');
+if (numero >= 100) {
+    alert('Parabéns, você venceu!');
+} else {
+    alert('Tente novamente para ganhar.');
+}
+
+saldoContaCorrente = 5000
+alert (`O saldo da sua conta corrente é de: ${saldoContaCorrente}`);
+
+nome = prompt('Insira seu nome:');
+alert (`Olá, ${nome}!`);
